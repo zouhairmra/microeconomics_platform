a74d4b24a193c76888a184fefc984a0000630ca7
zouhairmra <zouhairmrabet@gmail.com>
2026-02-21 21:58:24 +0300

    Update and rename LEADERBOARD to leaderboard.py

--- modules/leaderboard.py
+++ modules/leaderboard.py
@@ -6,14 +6,10 @@ def run():
 
     st.header("🏆 لوحة المتصدرين")
 
-    file="data/scores.csv"
+    file = "data/scores.csv"
 
     if os.path.exists(file):
-
-        df=pd.read_csv(file)
-
-        st.dataframe(df.sort_values("score",ascending=False))
-
+        df = pd.read_csv(file)
+        st.dataframe(df.sort_values("score", ascending=False))
     else:
-
         st.write("لا توجد نتائج بعد")
