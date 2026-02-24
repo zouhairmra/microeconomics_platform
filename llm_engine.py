95fc7e0d76c2a68a0a88ed36a49fa87213d85471
zouhairmra <zouhairmrabet@gmail.com>
2026-02-24 22:56:15 +0300

    Update llm_engine.py

--- llm_engine.py
+++ llm_engine.py
@@ -2,7 +2,7 @@ import openai
 import os
 
 def query_phi3(prompt):
-    api_key = os.getenv("OPENAI_API_KEY")
+    api_key = os.getenv("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")
     if not api_key:
         return "❌ OPENAI_API_KEY is missing. Check Streamlit Secrets."
 
