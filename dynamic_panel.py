019beb29bd1927c8d93584d60be45e14931873bc
zouhairmra <zouhairmrabet@gmail.com>
2026-02-24 21:59:08 +0300

    Update dynamic_panel.py

--- dynamic_panel.py
+++ dynamic_panel.py
@@ -1,8 +1,17 @@
 from linearmodels.panel import PanelOLS
 import statsmodels.api as sm
+def run_arellano_bond(df, entity, time, y_var, x_vars):
+    df = df.set_index([entity, time])
+    
+    # Create lagged dependent variable
+    df["lag_y"] = df.groupby(level=0)[y_var].shift(1)
+    df = df.dropna()
 
-def run_arellano_bond(df, y_var, x_vars):
     y = df[y_var]
-    X = df[x_vars]
-    model = ArellanoBond(y, X)
-    return model.fit()
+    X = df[["lag_y"] + x_vars]
+    X = sm.add_constant(X)
+
+    model = PanelOLS(y, X, entity_effects=True)
+    results = model.fit(cov_type="robust")
+
+    return results
