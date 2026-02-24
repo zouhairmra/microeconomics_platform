c18254bc518991fbf5114f994a080a47514bb73e
zouhairmra <zouhairmrabet@gmail.com>
2026-02-24 22:52:23 +0300

    Update llm_engine.py

--- llm_engine.py
+++ llm_engine.py
@@ -2,7 +2,7 @@ import openai
 import os
 
 def query_phi3(prompt):
-    client = openai.OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    client = openai.OpenAI(api_key=os.getenv("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"))
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[{"role": "user", "content": prompt}]
